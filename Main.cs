d8afeb75a8573c79c02b7379a6d3c0ca6d217929
thehambeard <61787444+thehambeard@users.noreply.github.com>
2023-09-25 20:13:59 -0400

    Refactor

--- Main.cs
+++ Main.cs
@@ -1,6 +1,4 @@
-﻿using ModMaker;
-using ModMaker.Utility;
-using System;
+﻿using System;
 using System.Reflection;
 using UnityModManagerNet;
 
@@ -11,16 +9,10 @@ namespace QuickCast
 #endif
     internal static class Main
     {
-        public static LocalizationManager<DefaultLanguage> Local;
-        public static ModManager<Core, Settings> Mod;
-        public static MenuManager Menu;
         public static string ModPath;
 
         private static bool Load(UnityModManager.ModEntry modEntry)
         {
-            Local = new LocalizationManager<DefaultLanguage>();
-            Mod = new ModManager<Core, Settings>();
-            Menu = new MenuManager();
             ModPath = modEntry.Path;
             modEntry.OnToggle = OnToggle;
 #if (DEBUG)
@@ -30,10 +22,6 @@ namespace QuickCast
 
         private static bool Unload(UnityModManager.ModEntry modEntry)
         {
-            Mod.Disable(modEntry, true);
-            Menu = null;
-            Mod = null;
-            Local = null;
             return true;
         }
 
@@ -44,27 +32,8 @@ namespace QuickCast
 
         private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
         {
-            if (value)
-            {
-                Assembly assembly = Assembly.GetExecutingAssembly();
-                Local.Enable(modEntry);
-                Mod.Enable(modEntry, assembly);
-                Menu.Enable(modEntry, assembly);
-            }
-            else
-            {
-                Menu.Disable(modEntry);
-                Mod.Disable(modEntry, false);
-                Local.Disable(modEntry);
-                ReflectionCache.Clear();
-            }
+            
             return true;
-        }
-
-        internal static Exception Error(string message)
-        {
-            Mod.Error(message);
-            return new InvalidOperationException(message);
         }
     }
 }
--- UI/QuickInventory/IViewChangeHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using Kingmaker.PubSubSystem;
-
-namespace QuickCast.UI.QuickInventory
-{
-    public interface IViewChangeHandler : ISubscriber, IGlobalSubscriber
-    {
-        void HandleViewChange();
-    }
-}
